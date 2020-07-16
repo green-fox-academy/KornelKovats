9d29f4ff41990cdb95bb5f6f0e959e70d716771f
Kornel Kovats <kornelkovats@Kornel-MacBook-Pro.local>
2020-07-16 13:26:02 +0200

    defender save 13:25

--- week1/day4/basicJSexercises/exercise.ts
+++ week1/day4/basicJSexercises/exercise.ts
@@ -97,3 +97,86 @@ console.log(b);
 let massInKg: number = 81.2;
 let heightInM: number = 1.78;
 
+console.log(massInKg/heightInM);
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as a number
+// Your height in meters as a number
+// Whether you are married or not as a boolean
+
+
+let myName: string = "Kornel";
+let age: number = 24;
+let height: number = 1.95;
+let married: boolean = false;
+
+console.log(myName);
+console.log(age);
+console.log(height);
+console.log(married);
+
+
+let aa: number = 3;
+// make it bigger by 10
+aa += 10;
+
+console.log(aa);
+
+let bb: number = 100;
+// make it smaller by 7
+bb -=8;
+
+console.log(bb);
+
+let cc: number = 44;
+// double c's value
+
+cc *= 2;
+
+console.log(cc);
+
+let dd: number = 125;
+// divide d's value by 5
+dd /= 5;
+
+console.log(dd);
+
+let ee: number = 8;
+// what's the cube of e's value?
+
+console.log(Math.pow(ee, 0.5));
+
+let f1: number = 123;
+let f2: number = 345;
+// tell if f1 is bigger than f2 (as a boolean)
+if (f1>f2){
+    console.log(true);
+}else {
+    console.log(false);
+}
+
+let g1: number = 350;
+let g2: number = 200;
+// tell if the double of g2 is bigger than g1 (as a boolean)
+if (g1<g2*2){
+    console.log(true);
+}else {
+    console.log(false);
+}
+
+let hh: number = 1357988018575474;
+// tell if 11 is a divisor of h (as a boolean)
+if (hh%11==0) {
+    console.log('yes it is divisor');
+}
+
+let i1: number = 10;
+let i2: number = 3;
+// tell if i1 is higher than i2 squared and smaller than i2 cubed (as a boolean)
+
+
+
+
+let j: number = 1521;
+// tell if j is divisible by 3 or 5 (as a boolean)
