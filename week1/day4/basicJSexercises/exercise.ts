87bf3b13f2b3c41abe30e71d8965103691ee76f6
Kornel Kovats <kornelkovats@Kornel-MacBook-Pro.local>
2020-07-16 13:07:33 +0200

    half of the practice ready

--- week1/day4/basicJSexercises/exercise.ts
+++ week1/day4/basicJSexercises/exercise.ts
@@ -10,9 +10,90 @@ console.log(3.141592);
 // Prints a boolean to the terminal window
 console.log(true);
 
+
+// Modify this program to console.log Humpty Dumpty riddle correctly
+
+console.log('All the king\'s horses and all the king\'s men');
+console.log('Humpty Dumpty had a great fall.');
+console.log('Humpty Dumpty sat on a wall,');
+console.log('Couldn\'t put Humpty together again.');
+
+// Write a program that prints a few details to the console about you
+// It should print each detail to a new line.
+//  - Your name
+//  - Your age
+//  - Your height in meters (as a decimal fraction)
+//
+//  Example output:
+//  John Doe
+//  31
+//  1.87
 // Store your favorite number in a variable (as a number)
 // And print it like this: 'My favorite number is: 8'
 
-let favNumber: number = 14;
 
-console.log("my favourite number is" + favNumber);
+console.log("Kornel Kovats");
+console.log("24");
+console.log(1.95);
+
+// Create a program that prints a few operations on two numbers: 22 and 13
+
+// Print the result of 13 added to 22
+
+// Print the result of 13 substracted from 22
+
+// Print the result of 22 multiplied by 13
+
+// Print the result of 22 divided by 13 (as a decimal fraction)
+
+// Print the remainder of 22 divided by 13
+
+let x: number = 13;
+let y: number = 22;
+
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(y/x);
+console.log(y%x);
+
+// An average Green Fox attendee codes 6 hours daily
+// The semester is 17 weeks long
+//
+// Print how many hours is spent with coding in a semester by an attendee,
+// if the attendee only codes on workdays.
+//
+// Print the percentage of the coding hours in the semester if the average
+// work hours weekly is 52
+
+
+console.log(17*5*6);
+console.log((17*30)/(17*52)*100 + "%");
+
+let favoriteNumber: number = 8;
+
+// Store your favorite number in a variable (as a number)
+// And print it like this: 'My favorite number is: 8'
+
+console.log('My favourite number is: ' + favoriteNumber);
+
+
+// Swap the values of these variables
+let a: number = 123;
+let b: number = 526;
+
+
+
+a= a+b;
+b = a - b;
+a = a - b;
+console.log(a);
+console.log(b);
+
+
+
+// Print the Body mass index (BMI) based on these values
+
+let massInKg: number = 81.2;
+let heightInM: number = 1.78;
+
