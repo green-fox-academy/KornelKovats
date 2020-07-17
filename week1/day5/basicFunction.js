dfb424f7cf518466afe87d37d6c20fd314208ac0
Kornel Kovats <kornelkovats@Kornel-MacBook-Pro.local>
2020-07-17 10:32:20 +0200

    day5 1st commit

---
+++ week1/day5/basicFunction.js
@@ -0,0 +1,5 @@
+let somestring = "something";
+
+console.log(somestring,somestring);
+
+l
---
+++ week1/day5/materialreview.ts
@@ -0,0 +1,14 @@
+let myArray = [];
+
+
+myArray[0] = 'cccc';
+
+myArray[1] = 'bbbb';
+
+
+
+
+
+
+myArray.sort();
+console.log(myArray);
