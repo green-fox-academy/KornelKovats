db0e1b0ea32a438d6afed094d6b71490f8f1d842
Kornel Kovats <kornelkovats@Kornel-MacBook-Pro.local>
2020-07-21 16:23:47 +0200

    telephone book ready

---
+++ week-02/day-02/Data_Sctructure/telephone-book.ts
@@ -0,0 +1,26 @@
+let object: any = {
+    'William A. Lathan': "405-709-1865",
+    'John K. Miller': "402-247-8568",
+    'Hortensia E. Foster': "606-481-6467",
+    'Amanda D. Newland': "319-243-5613",
+    'Brooke P. Askew': '307-687-2982'
+
+};
+let doweknow: boolean= false;
+
+Object.entries(object).forEach(function (value, index, arr) {
+    if (value[0]=='John K. Miller'){
+        console.log("John's phone is: " + value[1]);
+    }
+    if (value[1]== '307-687-2982'){
+        console.log("phone number is: " + value[0]);
+    }if (value[0]=="Chris E. Myers"&&value[1]!=undefined){
+        doweknow= true;
+    }
+});
+
+if (doweknow){
+    console.log("We know Chris E. Myers phone number");
+} else  {
+    console.log("We dont know Chris E. Myers phone number")
+}
