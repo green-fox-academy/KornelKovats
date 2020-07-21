454f8d6b983177e474a3f03e67ef9288de00627d
Kornel Kovats <kornelkovats@Kornel-MacBook-Pro.local>
2020-07-21 13:54:37 +0200

    list introduction2 ready

---
+++ week-02/day-02/Data_Sctructure/list-introduction2.js
@@ -0,0 +1,13 @@
+let A= ['Apple', 'Avocado', 'Blueberries', 'Durian', 'Lychee'];
+
+let B = A;
+
+//console.log(B);
+
+let seged
+
+if (A.includes("Durian")){
+    console.log(true);
+}else {
+    console.log(false);
+}
--- week-02/day-02/Data_Sctructure/list-introduction2.ts
+++ week-02/day-02/Data_Sctructure/list-introduction2.ts
@@ -1,1 +1,43 @@
-let string: string[]= ['Apple', 'Avocado', 'Blueberries', 'Durian', 'Lychee'];
+let A: string[]= ['Apple', 'Avocado', 'Blueberries', 'Durian', 'Lychee'];
+
+let B = A.map(function (value, index, array) {
+    return value;
+});
+
+//console.log(B);
+
+
+if (A.includes("Durian")){
+    console.log("Contains");
+}else {
+    console.log("Not Contains");
+}
+let index = B.indexOf("Durian");
+
+delete B[index];
+console.log(A);
+//console.log(B);
+
+
+A.splice(4,0,"Kiwifruit");
+
+
+console.log(A);
+
+let whichisBigger: string='';
+
+if (Object.keys(A).length>Object.keys(B).length){
+    console.log("A is bigger than B");
+} else {
+    console.log("B is bigger than A or equals");
+}
+
+
+console.log(A.indexOf('Avocado'));
+console.log(B.indexOf('Durian'));
+
+
+B.push('Passion Fruit');
+B.push('Pomelo');
+
+console.log(A[2]);
