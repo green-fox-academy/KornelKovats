0125e9c621a7bf7edf3ef243444629b8e44eeace
Kornel Kovats <kornelkovats@Kornel-MacBook-Pro.local>
2020-07-22 15:00:57 +0200

    write-single-time

---
+++ week-02/day-03/write-single-line.ts
@@ -0,0 +1,19 @@
+declare var require: any
+const fs = require('fs');
+
+
+// Write a function that is able to manipulate a file
+// By writing your name into it as a single line
+// The file should be named "my-file.txt"
+// In case the program is unable to write the file,
+// It should print the following error message: "Unable to write file: my-file.txt"
+
+
+let manipulate = (myName: string)=>{
+    try {
+        fs.writeFileSync('my-file.txt',myName)
+    }catch (e) {
+        console.log("Unable to write file: my-file.txt");
+    }
+}
+manipulate("Kornel");
