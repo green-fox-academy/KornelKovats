c2a17cfacd8cd4da5e5142ca71470c2e79057f5e
Kornel Kovats <kornelkovats@Kornel-MacBook-Pro.local>
2020-07-22 15:37:21 +0200

    copy file on stage

---
+++ week-02/day-03/copy-file.ts
@@ -0,0 +1,19 @@
+declare var require: any
+const fs = require('fs');
+// Write a function that copies the contents of a file into another
+// It should take the filenames as parameters
+// It should return a boolean that shows if the copy was successful
+
+let read = (filename:string)=> {
+
+   return fs.readFileSync(filename, 'utf-8');
+
+}
+
+
+
+let copy = (readfile:string)=>{
+    return fs.writeFileSync('copy.txt',readfile);
+};
+
+copy(read('asd.txt'));
