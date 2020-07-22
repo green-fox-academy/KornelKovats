ca53fbaaa53a33283a307739327ad673eb9f4e25
Kornel Kovats <kornelkovats@Kornel-MacBook-Pro.local>
2020-07-22 16:31:20 +0200

    logs

---
+++ week-02/day-03/logs.ts
@@ -0,0 +1,17 @@
+// Read all data from 'log.txt'.
+// Each line represents a log message from a web server
+// Write a function that returns an array with the unique IP adresses.
+// Write a function that returns the GET / POST request ratio.
+
+
+declare var require: any
+const fs = require('fs');
+
+let lines = fs.readFileSync('log.txt','utf-8').split('\n').map(function (value:string,index:number,arr:string[]) {
+    //console.log(value.split(' ')[5];
+    //console.log(value.split('   ')[1]);
+    return value.split('   ')[1];
+    //return value;
+});
+
+console.log(lines);
