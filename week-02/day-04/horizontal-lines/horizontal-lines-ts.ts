0750dd6edd9ba54b95bde1ac6488d9415602486f
Kornel Kovats <kornelkovats@Kornel-MacBook-Pro.local>
2020-07-23 13:11:00 +0200

    horiontal lines on stage

--- week-02/day-04/horizontal-lines/horizontal-lines-ts.ts
+++ week-02/day-04/horizontal-lines/horizontal-lines-ts.ts
@@ -7,20 +7,21 @@ const ctx = canvas.getContext('2d');
 // DO NOT TOUCH THE CODE ABOVE THIS LINE
 
 // Create a function that draws a single line and takes 2 parameters:
-// The x and y coordinates of the line's starting point
-// and draws a line from that point to the center of the canvas
+// the x and y coordinates of the line's starting point
+// and draws a 50 long horizontal line from that point.
 // Draw at least 3 lines with that function using a loop.
+
 
 function drawer (x:number,y:number) {
     // Draw a colored line
     ctx.strokeStyle = 'pink';
     ctx.beginPath();
     ctx.moveTo(x, y);
-    ctx.lineTo(canvas.width/2, canvas.height/2);
+    ctx.lineTo(x+(canvas.width-x), y);
     ctx.stroke();
 }
 
-for (let i = 0; i < 200; i++) {
+for (let i = 0; i < 3; i++) {
     let helper1 = Math.floor(Math.random() * 101);
     let helper2 = Math.floor(Math.random() * 101);
     drawer(helper1*6,helper2*4);
