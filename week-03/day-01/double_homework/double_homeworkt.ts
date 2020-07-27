3a32b3713186bcae5dbd56f36a3f2da8146a3cef
KornelKovats <kovats.kornel95@gmail.com>
2020-07-27 08:59:55 +0200

    homework

---
+++ week-03/day-01/double_homework/double_homeworkt.ts
@@ -0,0 +1,31 @@
+let tree1 = {
+    type: "Pine",
+    leafColor : "Brown",
+    age       : 15,
+    sex: "famale"
+};
+
+let tree2 = {
+    type: "Pine",
+    leafColor : "Brown",
+    age       : 16,
+    sex: "male"
+};
+let tree3 = {
+    type: "Pine",
+    leafColor : "Brown",
+    age       : 17,
+    sex: "famale"
+};
+let tree3 = {
+    type: "Pine",
+    leafColor : "Brown",
+    age       : 15,
+    sex: "male"
+};
+let tree5 = {
+    type: "Pine",
+    leafColor : "Brown",
+    age       : 17,
+    sex: "famale"
+};
