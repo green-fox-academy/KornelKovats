1471815a1b02415dd25809b9e06f63532b5582eb
KornelKovats <kovats.kornel95@gmail.com>
2020-08-28 14:27:18 +0200

    clicking ready for gallery project

--- week-06/day-05_gallery/app.js
+++ week-06/day-05_gallery/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { unwatchFile } = require('fs');
 
 const app = express();
 
@@ -6,11 +7,34 @@ app.set('view engine', 'ejs');
 app.use('/static', express.static('static'));
 const listOfPictures = ['1.jpeg', '2.jpeg', '3.jpeg', '4.jpeg', '5.jpeg'];
 
-const pictureObj = {
-  picture: listOfPictures,
-};
 app.get('/', (req, res) => {
-  res.render('home.ejs');
+  const data = {
+    pictures: listOfPictures,
+  };
+  let previousIndex = 0;
+  let nextIndex = 0;
+  let { imageIndex } = req.query;
+  if (imageIndex === undefined) {
+    imageIndex = 0;
+  } else {
+    imageIndex = Number(imageIndex);
+  }
+  if (imageIndex === 0) {
+    previousIndex = listOfPictures.length - 1;
+  } else {
+    previousIndex = imageIndex - 1;
+  }
+  if (imageIndex === listOfPictures.length - 1) {
+    nextIndex = 0;
+  } else {
+    nextIndex = imageIndex + 1;
+  }
+
+  data.currentImageUrl = `/static/${listOfPictures[imageIndex]}`;
+  data.previousIndex = previousIndex.toString();
+  data.nextIndex = nextIndex.toString();
+
+  res.render('home.ejs', data);
 });
 
 app.listen(3000, () => {
