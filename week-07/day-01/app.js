7afaed8ec16a203934e7aa9563a8174271d2490e
KornelKovats <kovats.kornel95@gmail.com>
2020-08-31 15:10:12 +0200

    frontend exercise ready

--- week-07/day-01/app.js
+++ week-07/day-01/app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const path = require('path');
 
+const bodyParser = require('body-parser');
+const { isLabeledStatement } = require('typescript');
+
 const app = express();
 const PORT = 3000;
 
 app.use(express.static('assets'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
@@ -20,6 +25,44 @@ app.get('/doubling', (req, res) => {
     res.send({ error: 'Please provide an input!' });
   }
   res.send(data);
+});
+
+app.get('/greeter', (req, res) => {
+  const { name, title } = req.query;
+
+  if (name === undefined && title === undefined) {
+    res.status(400).send({ error: 'Please provide a name and a title!' });
+  } else if (name === undefined && title !== undefined) {
+    res.status(400).send({ error: 'Please provide a name!' });
+  } else if (name !== undefined && title === undefined) {
+    res.status(400).send({ error: 'Please provide a title!' });
+  }
+  res.send({ welcome_message: `Oh, hi there ${name}, my dear ${title}!` });
+});
+
+app.get('/appenda/:appended', (req, res) => {
+  res.send({ appended: `${req.params.appended}a` });
+});
+
+app.post('/dountil/:action', (req, res) => {
+  const { action } = req.params;
+  const number = parseInt(req.body.until, 10);
+  let responseNumber = 0;
+  if (action === 'sum') {
+    for (let i = 1; i <= number; i++) {
+      responseNumber += i;
+    }
+    res.send({ result: responseNumber });
+  } else if (action === 'factor') {
+    responseNumber = 1;
+    for (let i = 1; i <= number; i++) {
+      responseNumber *= i;
+    }
+    res.send({ result: responseNumber });
+  } else if (number === undefined) {
+    res.send({ error: 'Please provide a number!' });
+  }
+  res.send(number);
 });
 
 app.listen(PORT, () => {
