e6b0afbc08e74cdc025865010306c70cc282bf80
KornelKovats <kovats.kornel95@gmail.com>
2020-09-03 13:20:29 +0200

    route upvote ready and working

--- week-07/day-04_reddit/routes.js
+++ week-07/day-04_reddit/routes.js
@@ -23,9 +23,34 @@ router.post('/posts', (req, res) => {
       res.sendStatus(500);
       return null;
     }
-    return res.status(200).json(rows);
+    conn.query(`SELECT * FROM posts where id=${rows.insertId}`, (err, rows) => {
+      if (err) {
+        console.error(`Cannot retrieve data: ${err.toString()}`);
+        res.sendStatus(500);
+        return null;
+      }
+      return res.status(200).json(rows);
+    });
   });
-  console.log(post);
+});
+
+router.put('/posts/:id/upvote', (req, res) => {
+  const id = [req.params.id];
+  conn.query('UPDATE posts set score=score+1 where id=?;', id, (err, rows) => {
+    if (err) {
+      console.error(`Cannot retrieve data: ${err.toString()}`);
+      res.sendStatus(500);
+      return null;
+    }
+    conn.query('SELECT * FROM posts where id=?', id, (err, rows) => {
+      if (err) {
+        console.error(`Cannot retrieve data: ${err.toString()}`);
+        res.sendStatus(500);
+        return null;
+      }
+      return res.status(200).json(rows);
+    });
+  });
 });
 
 module.exports = router;
