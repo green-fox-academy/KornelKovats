6d30f83404dbd9b81b156dc8cfc854708235f09b
KornelKovats <kovats.kornel95@gmail.com>
2020-09-03 13:28:30 +0200

    votes ready starting the frontend.js file

--- week-07/day-04_reddit/routes.js
+++ week-07/day-04_reddit/routes.js
@@ -52,5 +52,23 @@ router.put('/posts/:id/upvote', (req, res) => {
     });
   });
 });
+router.put('/posts/:id/downvote', (req, res) => {
+  const id = [req.params.id];
+  conn.query('UPDATE posts set score=score-1 where id=?;', id, (err, rows) => {
+    if (err) {
+      console.error(`Cannot retrieve data: ${err.toString()}`);
+      res.sendStatus(500);
+      return null;
+    }
+    conn.query('SELECT * FROM posts where id=?', id, (err, rows) => {
+      if (err) {
+        console.error(`Cannot retrieve data: ${err.toString()}`);
+        res.sendStatus(500);
+        return null;
+      }
+      return res.status(200).json(rows);
+    });
+  });
+});
 
 module.exports = router;
