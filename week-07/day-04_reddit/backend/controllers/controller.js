29423c16e4991378137717887edf7443fb0fce71
KornelKovats <kovats.kornel95@gmail.com>
2020-09-08 17:36:36 +0200

    lollipop needs to implement candy rain

--- week-07/day-04_reddit/backend/controllers/controller.js
+++ week-07/day-04_reddit/backend/controllers/controller.js
@@ -8,27 +8,32 @@ router.use(express.json());
 router.get('/posts', (req, res) => {
   if (req.headers.username !== undefined) {
     // select user from users
-    conn.query(`SELECT * FROM users WHERE username='${req.headers.username}';`, (err, rows) => {
-      if (err) {
-        console.error(`Cannot retrieve data: ${err.toString()}`);
-        res.sendStatus(500);
-        return null;
-      }
-      // handle if user exists
-      if (rows.length !== 0) {
-        conn.query(`SELECT posts.id, posts.title, posts.url, posts.date_time, posts.score, username as owner,(SELECT vote FROM votes WHERE votes.user_id='${rows[0].id}' and votes.post_id=posts.id) as vote FROM posts LEFT JOIN users ON users.id=posts.id_user;`,
-          (err, rows) => {
-            if (err) {
-              console.error(`Cannot retrieve data: ${err.toString()}`);
-              res.sendStatus(500);
-              return null;
-            }
-            return res.status(200).json(rows);
-          });
-      } else {
-        res.status(404).send('Username doesn\'t exist');
-      }
-    });
+    conn.query(
+      `SELECT * FROM users WHERE username='${req.headers.username}';`,
+      (err, rows) => {
+        if (err) {
+          console.error(`Cannot retrieve data: ${err.toString()}`);
+          res.sendStatus(500);
+          return null;
+        }
+        // handle if user exists
+        if (rows.length !== 0) {
+          conn.query(
+            `SELECT posts.id, posts.title, posts.url, posts.date_time, posts.score, username as owner,(SELECT vote FROM votes WHERE votes.user_id='${rows[0].id}' and votes.post_id=posts.id) as vote FROM posts LEFT JOIN users ON users.id=posts.id_user;`,
+            (err, rows) => {
+              if (err) {
+                console.error(`Cannot retrieve data: ${err.toString()}`);
+                res.sendStatus(500);
+                return null;
+              }
+              return res.status(200).json(rows);
+            },
+          );
+        } else {
+          res.status(404).send("Username doesn't exist");
+        }
+      },
+    );
   } else {
     // TODO: fix HTTP status code
     res.status(404).send('Can not get posts without username');
@@ -38,36 +43,50 @@ router.get('/posts', (req, res) => {
 router.post('/posts', (req, res) => {
   req.accepts('application/json');
   // eslint-disable-next-line max-len
-  if (req.headers.username !== undefined && req.body.title !== undefined && req.body.url !== undefined) {
+  if (
+    req.headers.username !== undefined
+    && req.body.title !== undefined
+    && req.body.url !== undefined
+  ) {
     // select user from users
-    conn.query(`SELECT * FROM users WHERE username='${req.headers.username}';`, (err, rows) => {
-      if (err) {
-        console.error(`Cannot retrieve data: ${err.toString()}`);
-        res.sendStatus(500);
-        return null;
-      }
-      // handle if user exists
-      if (rows[0].length !== 0) {
-        const insertedUserValue = [req.body.title, req.body.url, rows[0].id];
-        conn.query('INSERT INTO posts (title,url,id_user) values (?,?,?);', insertedUserValue, (err, rows) => {
-          if (err) {
-            console.error(`Cannot retrieve data: ${err.toString()}`);
-            res.sendStatus(500);
-            return null;
-          }
-          conn.query(`SELECT * FROM posts where id='${rows.insertId}';`, (err, rows) => {
-            if (err) {
-              console.error(`Cannot retrieve data: ${err.toString()}`);
-              res.sendStatus(500);
-              return null;
-            }
-            return res.status(200).json(rows);
-          });
-        });
-      } else {
-        res.status(404).send('Username doesn\'t exist');
-      }
-    });
+    conn.query(
+      `SELECT * FROM users WHERE username='${req.headers.username}';`,
+      (err, rows) => {
+        if (err) {
+          console.error(`Cannot retrieve data: ${err.toString()}`);
+          res.sendStatus(500);
+          return null;
+        }
+        // handle if user exists
+        if (rows[0].length !== 0) {
+          const insertedUserValue = [req.body.title, req.body.url, rows[0].id];
+          conn.query(
+            'INSERT INTO posts (title,url,id_user) values (?,?,?);',
+            insertedUserValue,
+            (err, rows) => {
+              if (err) {
+                console.error(`Cannot retrieve data: ${err.toString()}`);
+                res.sendStatus(500);
+                return null;
+              }
+              conn.query(
+                `SELECT * FROM posts where id='${rows.insertId}';`,
+                (err, rows) => {
+                  if (err) {
+                    console.error(`Cannot retrieve data: ${err.toString()}`);
+                    res.sendStatus(500);
+                    return null;
+                  }
+                  return res.status(200).json(rows);
+                },
+              );
+            },
+          );
+        } else {
+          res.status(404).send("Username doesn't exist");
+        }
+      },
+    );
   } else {
     // TODO: fix HTTP status code
     res.status(404).send('Can not make post without username or title or url');
@@ -137,21 +156,25 @@ router.put('/posts/:id', (req, res) => {
   req.accepts('application/json');
   const whatNeedstoModified = [req.body.title, req.body.url, req.params.id];
   const id = [req.params.id];
-  conn.query('UPDATE posts SET title=?, url=? where id=?;', whatNeedstoModified, (err, rows) => {
-    if (err) {
-      console.error(`Cannot retrieve data: ${err.toString()}`);
-      res.sendStatus(500);
-      return null;
-    }
-    conn.query('SELECT * FROM posts where id=?', id, (err, rows) => {
+  conn.query(
+    'UPDATE posts SET title=?, url=? where id=?;',
+    whatNeedstoModified,
+    (err, rows) => {
       if (err) {
         console.error(`Cannot retrieve data: ${err.toString()}`);
         res.sendStatus(500);
         return null;
       }
-      return res.status(200).json(rows);
-    });
-  });
+      conn.query('SELECT * FROM posts where id=?', id, (err, rows) => {
+        if (err) {
+          console.error(`Cannot retrieve data: ${err.toString()}`);
+          res.sendStatus(500);
+          return null;
+        }
+        return res.status(200).json(rows);
+      });
+    },
+  );
 });
 
 module.exports = router;
