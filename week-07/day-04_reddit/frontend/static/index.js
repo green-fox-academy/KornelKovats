153b038cb12885d1587e4749a905aa5c7e7a3cc2
KornelKovats <kovats.kornel95@gmail.com>
2020-09-14 09:36:01 +0200

    downvote upvote png s inserted to html

--- week-07/day-04_reddit/frontend/static/index.js
+++ week-07/day-04_reddit/frontend/static/index.js
@@ -1,27 +1,20 @@
 function reqListener() {
-  const data = JSON.parse((this.responseText));
-
-  const table = document.querySelector('table');
-  data.forEach((element) => {
-    const row = document.createElement('tr');
-    row.style.width = '700px';
-    const score = document.createElement('td');
-    const title = document.createElement('td');
-    score.innerText = element.score;
-    title.innerText = element.title;
-    row.appendChild(score);
-    row.appendChild(title);
-    table.appendChild(row);
-
-  });
-  console.log(data);
+  const posts = JSON.parse((this.responseText));
+createPosts({posts: posts});
 }
+function createPosts(posts) {
+  const rawTemplate = document.getElementById('postsTemplate').innerHTML;
+  const compiledTemplate = Handlebars.compile(rawTemplate);
+  const myHTML = compiledTemplate(posts);
 
+  const posts_body = document.querySelector('.posts_body');
+  posts_body.innerHTML = myHTML;
+}
 const xhr = new XMLHttpRequest();
-
 xhr.addEventListener('load', reqListener);
-
 xhr.open('GET', 'http://localhost:3000/api/posts', true);
 xhr.setRequestHeader('Content-Type', 'application/json');
 xhr.setRequestHeader('username', 'Kornel');
 xhr.send();
+
+
