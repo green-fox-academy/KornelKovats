5f772eb13c0cd1c46c12b0ac84d732422c5deb90
KornelKovats <kovats.kornel95@gmail.com>
2020-09-15 15:35:19 +0200

    yondu endpoint tested

--- week-09/day-02/iamgroot/test/test-routes.js
+++ week-09/day-02/iamgroot/test/test-routes.js
@@ -31,3 +31,34 @@ test('groot endpoint with query string', (t) => {
       t.end();
     });
 });
+
+test('yondu endpoint without query string', (t) => {
+  request(app)
+    .get('/yondu')
+    .expect(200)
+    .expect('Content-Type', /json/)
+    .end((err, res) => {
+      const expectedThings = { error: 'can not calculate the speed' };
+      const actualThings = res.body;
+      t.error(err, 'No error');
+      t.same(actualThings, expectedThings, 'Retrieve list of things');
+      t.end();
+    });
+});
+test('yondu endpoint with query string', (t) => {
+  request(app)
+    .get('/yondu?distance=100.0&time=10.0')
+    .expect(200)
+    .expect('Content-Type', /json/)
+    .end((err, res) => {
+      const expectedThings = {
+        distance: 100.0,
+        time: 10.0,
+        speed: 10.0,
+      };
+      const actualThings = res.body;
+      t.error(err, 'No error');
+      t.same(actualThings, expectedThings, 'Retrieve list of things');
+      t.end();
+    });
+});
