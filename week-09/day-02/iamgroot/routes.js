479660edfb404cfd617e1c9b4aa143c2b3c9ba26
KornelKovats <kovats.kornel95@gmail.com>
2020-09-15 15:37:17 +0200

    nothing file addedto week5day2

--- week-09/day-02/iamgroot/routes.js
+++ week-09/day-02/iamgroot/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { parse } = require('path');
 
 const app = express();
 
@@ -12,6 +13,20 @@ app.get('/groot', (req, res) => {
       received: req.query.message,
       translated: 'I am Groot!',
     });
+  }
+});
+app.get('/yondu', (req, res) => {
+  const disTance = parseInt(req.query.distance, 10);
+  const tiMe = parseInt(req.query.time, 10);
+  const spEEd = disTance/tiMe;
+  if (req.query.distance !== undefined && req.query.time !== undefined) {
+    res.json({
+      distance: disTance,
+      time: tiMe,
+      speed: spEEd,
+    });
+  } else {
+    res.send({ error: 'can not calculate the speed' });
   }
 });
 
