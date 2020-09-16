d40831bdab201d39008f52ebe7e01ad7db52d8cf
KornelKovats <kovats.kornel95@gmail.com>
2020-09-16 15:44:49 +0200

    exercise 1.0 ready

--- week-09/day-02/iamgroot/routes.js
+++ week-09/day-02/iamgroot/routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { parse } = require('path');
+const conn = require('./connectionDB/connection');
 
 const app = express();
 
@@ -18,7 +19,7 @@ app.get('/groot', (req, res) => {
 app.get('/yondu', (req, res) => {
   const disTance = parseInt(req.query.distance, 10);
   const tiMe = parseInt(req.query.time, 10);
-  const spEEd = disTance/tiMe;
+  const spEEd = disTance / tiMe;
   if (req.query.distance !== undefined && req.query.time !== undefined) {
     res.json({
       distance: disTance,
@@ -29,5 +30,16 @@ app.get('/yondu', (req, res) => {
     res.send({ error: 'can not calculate the speed' });
   }
 });
-
+app.get('/rocket', (req, res) => {
+  conn.query('SELECT * FROM rocket', (err, rows) => {
+    if (err) {
+      console.error(`Cannot retrieve data: ${err.toString()}`);
+      res.sendStatus(500);
+      return null;
+    }
+    
+    console.log(rows[0]);
+    res.json(rows);
+  });
+});
 module.exports = app;
