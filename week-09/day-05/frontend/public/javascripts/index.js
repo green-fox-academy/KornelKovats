f9567be7cbb04c752ed57f8674a622255c8f6325
KornelKovats <kovats.kornel95@gmail.com>
2020-09-18 15:41:28 +0200

    the game works need to make manage questions

--- week-09/day-05/frontend/public/javascripts/index.js
+++ week-09/day-05/frontend/public/javascripts/index.js
@@ -1,14 +1,34 @@
+const card = document.querySelector('.card');
+let score = 0;
+const h1 = document.querySelector('h1');
+
 window.addEventListener('load', loadQuestion);
 function loadQuestion() {
+  h1.innerText = `SCORE: ${score}`;
+  const answers = document.querySelectorAll('.answer');
+
   fetch('http://localhost:3000/api/game')
     .then((response) => response.json())
     .then((data) => {
       document.querySelector('.question').innerHTML = data.question;
-      data.answers.forEach((element, index)=>{
-          console.log(element);
+      h1.innerText = `SCORE: ${score}`;
+      data.answers.forEach((element, index) => {
+        document.getElementById(`${index}`).setAttribute('style', 'background-color: #fff;');
         document.getElementById(`${index}`).innerHTML = element.answer;
-      })
-
-      //onsole.log(data);
+        document.getElementById(`${index}`).dataset.is_correct = element.is_correct;
+      });
     });
 }
+
+card.addEventListener('click', (event) => {
+  if (event.target.className === 'answer') {
+    if (event.target.dataset.is_correct === '1') {
+      score++;
+      event.target.style.backgroundColor = '#A5FF91';
+      setTimeout(loadQuestion, 400);
+    } else {
+      event.target.style.backgroundColor = '#FF7878';
+      setTimeout(loadQuestion, 400);
+    }
+  }
+});
