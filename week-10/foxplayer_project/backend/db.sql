5fa9ca42dead70ddeff318978c246d5d0691c0d3
KornelKovats <kovats.kornel95@gmail.com>
2020-09-22 15:20:20 +0200

    sql file ready

---
+++ week-10/foxplayer_project/backend/db.sql
@@ -0,0 +1,16 @@
+CREATE SCHEMA foxplayer;
+
+CREATE TABLE playlists(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	playlist TINYTEXT NOT NULL,
+	system_ TINYINT(1) DEFAULT 0
+);
+
+CREATE TABLE tracks(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	path TINYTEXT NOT NULL,
+	playlist_id INT,
+		FOREIGN KEY (playlist_id)
+		REFERENCES playlists(id)
+);
+
